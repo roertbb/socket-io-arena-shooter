bcf910aa86cf87e6a8d12d1c757e4001b01a8846
roertbb <robert.banaszak1997@gmail.com>
2017-09-25 07:23:32 +0200

    working targeting

--- public/script.js
+++ public/script.js
@@ -13,10 +13,18 @@ let Key = {
 	keys: {w: 87, s: 83, a: 65, d: 68},
 	isDown: function (keyCode) { return this.pressed[this.keys[keyCode]]; },
 	onKeyDown: function (event) { this.pressed[event.keyCode] = true; },
-	onKeyUp: function (event) { delete this.pressed[event.keyCode]; }
+    onKeyUp: function (event) { delete this.pressed[event.keyCode]; },
+    mouse: {x: 0, y: 0}
 };
 window.addEventListener('keyup', (event) => { Key.onKeyUp(event); }, false);
 window.addEventListener('keydown', (event) => { Key.onKeyDown(event); }, false);
+window.addEventListener('mousemove', (event) => { getMouseCords(event); }, false);
+
+function getMouseCords(event) {
+    let rect = canvas.getBoundingClientRect();
+    Key.mouse.x = event.clientX-rect.left;
+    Key.mouse.y = event.clientY-rect.top;
+}
 
 let players = [];
 let map = undefined;
@@ -25,7 +33,8 @@ let tick = 0;
 function move() {
     let player = {
         id: socket.id,
-        down: []
+        down: [],
+        mouse: {x: Key.mouse.x, y: Key.mouse.y}
     };
 
     if (Key.isDown('w'))
@@ -37,7 +46,7 @@ function move() {
     else if (Key.isDown('d'))
         player.down.push('d');
 
-    if (player.down.length !== 0)
+    // if (player.down.length !== 0)
         socket.emit('sendPlayerData', player);
 }
 
@@ -65,11 +74,32 @@ function draw() {
             ctx.fillStyle = "#4f3";
 
         ctx.fillRect(player.x, player.y, t, t);
+
+        if (player.mouse !== undefined) {
+            
+        let dy = player.mouse.y - player.y+10,
+            dx = player.mouse.x - player.x+10,
+            angle = Math.atan2(dy, dx);
+            
+        ctx.strokeStyle = "#000";
+        ctx.lineWidth = 3;
+        ctx.beginPath();
+        ctx.moveTo(player.x+10, player.y+10);
+        ctx.lineTo(player.x+10 + Math.cos(angle)*20, player.y+10 + Math.sin(angle)*20);
+        ctx.stroke();
+
+        }
     });
 
     ctx.font = "14px arial";
     ctx.fillStyle = "#fff";
-    ctx.fillText(`${tick}`, 5, 15);
+    ctx.fillText(`${tick} x:${Key.mouse.x} y:${Key.mouse.y}`, 5, 15);
+
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#fff";
+    ctx.beginPath();
+    ctx.arc(Key.mouse.x,Key.mouse.y,5,0,Math.PI*2);
+    ctx.stroke();
 }
 
 socket.on('emitPlayerData', data => {
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(express.static('public'));
 console.log("Starting server...");
 
 let players = [];
+let playersData = [];
 
 let map = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
@@ -17,11 +18,11 @@ let map = [
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1],
     [1,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1],
     [1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
@@ -30,14 +31,21 @@ const t = 20;
 
 io.on('connection', function(socket) {
     console.log("made socket connection", socket.id);
-    players.push({id: socket.id, x: 150, y: 150});
+    players.push({id: socket.id, x: 150, y: 150, vx: 0, vy: 0, ground: false});
+    playersData.push({id: socket.id, down: []});
 
     socket.emit('sendMap', {map: map});
     io.sockets.emit('emitPlayerData', players);
 
     socket.on('sendPlayerData', playerData => {
-        updatePlayer(playerData);
-        io.sockets.emit('emitPlayerData', players);
+        playersData.forEach(pd => {
+            if (pd.id === playerData.id) {
+                pd.down = playerData.down;
+                pd.mouse = playerData.mouse;
+            }
+        });
+        // updatePlayer(playerData);
+        // io.sockets.emit('emitPlayerData', players);
     });
 
     socket.on('disconnect', function(){
@@ -47,36 +55,135 @@ io.on('connection', function(socket) {
     })
 });
 
-function updatePlayer(playerData) {
-    let cords = {x:0, y:0}
-    if (playerData.down.indexOf('w') !== -1)
-        cords.y = -2;
-    else if (playerData.down.indexOf('s') !== -1)
-        cords.y = 2;
-    if (playerData.down.indexOf('a') !== -1)
-        cords.x = -2;
-    else if (playerData.down.indexOf('d') !== -1)
-        cords.x = 2;
+// function updatePlayer(playerData) {
+//     let cords = {x:0, y:0}
+//     if (playerData.down.indexOf('w') !== -1)
+//         cords.y = -2;
+//     else if (playerData.down.indexOf('s') !== -1)
+//         cords.y = 2;
+//     if (playerData.down.indexOf('a') !== -1)
+//         cords.x = -2;
+//     else if (playerData.down.indexOf('d') !== -1)
+//         cords.x = 2;
     
-    players.forEach(player => {
-        if (playerData.id == player.id) {
-            if (!collTile(player.x+cords.x, player.y))
-                player.x += cords.x;
-            if (!collTile(player.x, player.y+cords.y))
-                player.y += cords.y;
+//     players.forEach(player => {
+//         if (playerData.id == player.id) {
+//             if (!collTile(player.x+cords.x, player.y))
+//                 player.x += cords.x;
+//             if (!collTile(player.x, player.y+cords.y))
+//                 player.y += cords.y;
+//         }
+//     });
+// }
+
+function updatePlayer(playerData) {
+    // console.log(playerData);
+    let player = players.filter(p => p.id === playerData.id)[0];
+    let down = playerData.down;
+    let mouse = playerData.mouse;
+    
+    if (down === undefined)
+        down = [];
+
+    // console.log(down);
+
+    if (player !== undefined) {
+
+    if (down.indexOf('a') !== -1) {
+        if (player.vx > -5)
+            player.vx -= 0.5;
+    }
+    else if (down.indexOf('d') !== -1) {
+        if (player.vx < 5)
+            player.vx += 0.5;
+    }
+    else {
+        if (player.vx > 0)
+            player.vx -= 0.5;
+        else if (player.vx < 0)
+            player.vx += 0.5;
+    }
+
+    if (down.indexOf('w') !== -1 && player.ground) {
+        player.vy = -9.5;
+        player.ground = false;
+    }
+
+    player.ground = false;
+    if (collTile(player.x, player.y+1))
+        player.ground = true;
+    if (!player.ground) {
+            player.vy+=0.8;
+    }
+
+    if (!collTile(player.x+player.vx, player.y))
+        player.x += player.vx;
+    else {
+        do {
+            if (player.vx > 0)
+                player.vx -= 0.5;
+            else if (player.vx < 0)
+                player.vx += 0.5;
+        } while(collTile(player.x+player.vx, player.y))
+        player.x += player.vx;
+    }
+
+    if (!collTile(player.x, player.y+player.vy))
+        player.y += player.vy;
+    else {
+        do {
+            if (player.vy > 0)
+                player.vy -= 0.5;
+            else if (player.vy < 0)
+                player.vy += 0.5;
+        } while(collTile(player.x, player.y + player.vy))
+        player.y += player.vy;
+        if (player.vy > 0)
+            player.ground = true;
+        player.vy = 0;
+    }
+
+    players.forEach(p => {
+        if (p.id == player.id) {
+            p.x = player.x;
+            p.y = player.y;
+            p.dx = player.dx;
+            p.dy = player.dy;
+            p.ground = player.ground;
+            p.mouse = mouse;
         }
     });
+
+    }
 }
 
 function collTile(x, y) {
-    if (map[Math.floor(y/t)][Math.floor(x/t)] !== 0 ||
-        map[Math.floor((y+t-2)/t)][Math.floor(x/t)] !== 0 ||
-        map[Math.floor(y/t)][Math.floor((x+t-2)/t)] !== 0 ||
-        map[Math.floor((y+t-2)/t)][Math.floor((x+t-2)/t)] !== 0)
+    // if (map[Math.floor(y/t)][Math.floor(x/t)] !== 0 ||
+    //     map[Math.floor((y+t-2)/t)][Math.floor(x/t)] !== 0 ||
+    //     map[Math.floor(y/t)][Math.floor((x+t-2)/t)] !== 0 ||
+    //     map[Math.floor((y+t-2)/t)][Math.floor((x+t-2)/t)] !== 0)
+    //     return true;
+    // else
+    //     return false;
+    if (map[Math.floor(y/t)][Math.floor(x/t)] !== 0 || 
+        map[Math.ceil(y/t)][Math.floor(x/t)] !== 0 ||
+        map[Math.floor(y/t)][Math.ceil(x/t)] !== 0 ||
+        map[Math.ceil(y/t)][Math.ceil(x/t)] !== 0) 
         return true;
-    else
+    else 
         return false;
 }
+
+setInterval( function() {
+    players.forEach(player => {
+        let pd = playersData.filter(pd => pd.id == player.id)[0];
+        if (pd === undefined)
+            pd = {id: player.id, down: []};
+        updatePlayer(pd);
+    });
+
+    io.sockets.emit('emitPlayerData', players);
+}, 30);
 
 let tick = 0;
 setInterval( function() {
